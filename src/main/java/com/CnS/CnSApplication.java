3f40a1db6d2a621579a2d150804675b96c1fac9c
OhHongGi <5ghdrl@naver.com>
2022-11-19 00:06:08 +0900

    chore: Init spring boot

---
+++ src/main/java/com/CnS/CnSApplication.java
@@ -0,0 +1,13 @@
+package com.CnS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CnS/CnSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CnS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
