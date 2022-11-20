7438b368e10547bcb6429a439d04ad88da60ae02
kimdonghun <zhfptm12@naver.com>
2022-11-20 23:40:19 +0900

    Feat : student entity table

---
+++ src/main/java/com/CnS/domain/entity/Student.java
@@ -0,0 +1,24 @@
+package com.CnS.domain.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter @Setter
+@Entity
+@Table(name = "sns")
+public class Student {
+    @Id
+    private int id;
+
+    private String name;
+
+    private String password;
+
+
+}
