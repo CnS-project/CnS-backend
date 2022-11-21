e97151094f1bfcf4c02521fa86000798e6fad5ab
kimdonghun <zhfptm12@naver.com>
2022-11-21 20:08:38 +0900

    Feat : login dto class

---
+++ src/main/java/com/CnS/domain/dto/LoginDto.java
@@ -0,0 +1,12 @@
+package com.CnS.domain.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LoginDto {
+    private Integer studentId;
+    private String password;
+}
+
