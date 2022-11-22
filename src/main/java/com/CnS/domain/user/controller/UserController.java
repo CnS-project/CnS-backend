d4a9ad25ef70de8529119cd3cd0ffd318cd5b05d
kimdonghun <zhfptm12@naver.com>
2022-11-22 20:33:32 +0900

    Feat : add get regisered course list of user getMapping

--- src/main/java/com/CnS/domain/user/controller/UserController.java
+++ src/main/java/com/CnS/domain/user/controller/UserController.java
@@ -2,12 +2,15 @@ package com.CnS.domain.user.controller;
 
 import com.CnS.domain.user.dto.LoginDto;
 import com.CnS.domain.user.dto.RegisterCourseRequestDto;
+import com.CnS.domain.user.dto.coursesIdOfUserDto;
 import com.CnS.domain.user.entity.Student;
 import com.CnS.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,13 +27,13 @@ public class UserController {
 
     @PostMapping("/registration-course")
     public ResponseEntity<Void> registerCourse(
-        @RequestBody RegisterCourseRequestDto registerCourseRequestDto) {
+            @RequestBody RegisterCourseRequestDto registerCourseRequestDto) {
         userService.registerCourse(registerCourseRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-//    @GetMapping("/users/{student-id}/courses")
-//    public ResponseEntity<Student> inquire(@PathVariable("student-id") Integer id) {
-//
-//    }
+    @GetMapping("/{student-id}/courses")
+    public ResponseEntity<coursesIdOfUserDto> inquire(@PathVariable("student-id") Integer id) {
+        return ResponseEntity.ok(userService.courseList(id));
+    }
 }
