f92717e5fd335ed819c019fd9e0008fd3b04cb29
kimdonghun <zhfptm12@naver.com>
2022-11-23 00:44:04 +0900

    Refactor : error code add

--- src/main/java/com/CnS/global/error/exception/ErrorCode.java
+++ src/main/java/com/CnS/global/error/exception/ErrorCode.java
@@ -17,12 +17,14 @@ public enum ErrorCode {
     // Course
     DUPLICATE_COURSE(400, "C4001", "The course is existed"),
     COURSE_NOT_EXIST(400, "C4002", "The course isn't exist"),
+    OVER_CAPACITY(400, "C4003", "The capacity is full"),
 
     // User
     DUPLICATE_REGISTER(400, "U4001", "This course has already been registered.\n"),
     NONE_SESSION_INFORMATION(401, "U4011", "Session information does not exist"),
     INVALID_USER_ID(401, "U4012", "User id is invalid"),
     INVALID_USER_PASSWORD(401, "U4013", "password is invalid"),
+    OVER_CREDIT(400, "U4002", "Over Credit"),
 
 
     ;
