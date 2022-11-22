44a83a5299b957032bab528924d0ca5fbf2a103b
kimdonghun <zhfptm12@naver.com>
2022-11-22 20:53:46 +0900

    Feat : add sql query select courses with courseId

--- src/main/java/com/CnS/domain/course/repository/CourseRepository.java
+++ src/main/java/com/CnS/domain/course/repository/CourseRepository.java
@@ -2,9 +2,17 @@ package com.CnS.domain.course.repository;
 
 import com.CnS.domain.course.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
+
 public interface CourseRepository extends JpaRepository<Course, String> {
+
+    @Query("select c from Course c where c.courseId in :coursesId")
+    List<Course> findAllByCoursesId(@Param("coursesId") List<String> courses_Id);
 
 }
