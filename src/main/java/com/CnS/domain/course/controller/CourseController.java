0720958240b4d7a77d52d90d8965878f17d91f7d
OhHongGi <5ghdrl@naver.com>
2022-11-23 17:12:24 +0900

    refactor: Modify cors configuration

--- src/main/java/com/CnS/domain/course/controller/CourseController.java
+++ src/main/java/com/CnS/domain/course/controller/CourseController.java
@@ -8,6 +8,7 @@ import com.CnS.domain.course.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/courses")
 public class CourseController {
 
