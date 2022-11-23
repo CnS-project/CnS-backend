e969e8c534d8e675f64198e101ff7f3c3b4cff1f
OhHongGi <5ghdrl@naver.com>
2022-11-23 15:41:38 +0900

    chore: Add CourseModifyRequestDto file

---
+++ src/main/java/com/CnS/domain/course/dto/CourseModifyRequestDto.java
@@ -0,0 +1,29 @@
+package com.CnS.domain.course.dto;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+
+@Getter
+public class CourseModifyRequestDto {
+
+    private Integer targetGrade;
+
+    private Integer credit;
+
+    private String name;
+
+    @NotNull
+    private Integer courseNumber;
+
+    @NotNull
+    private Integer classNumber;
+
+    private String professor;
+
+    private Integer capacity;
+
+    private Integer applicant;
+
+    private String major;
+
+}
