532b4a6d9150a6bd4f2ffe1235901e31b65ab347
OhHongGi <5ghdrl@naver.com>
2022-11-23 15:44:44 +0900

    feat: Add ModifyCourse feature for administrator

--- src/main/java/com/CnS/domain/course/controller/CourseController.java
+++ src/main/java/com/CnS/domain/course/controller/CourseController.java
@@ -2,12 +2,14 @@ package com.CnS.domain.course.controller;
 
 import com.CnS.domain.course.dto.CourseInquiryResponseDto;
 import com.CnS.domain.course.dto.CourseInsertRequestDto;
+import com.CnS.domain.course.dto.CourseModifyRequestDto;
 import com.CnS.domain.course.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,13 @@ public class CourseController {
     public ResponseEntity<Void> insertCourse(@RequestBody CourseInsertRequestDto courseInsertRequestDto) {
         courseService.insertCourse(courseInsertRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Void> modifyCourse(
+        @RequestBody CourseModifyRequestDto courseModifyRequestDto) {
+        courseService.modifyCourse(courseModifyRequestDto);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
