1c1133ff507c74b8ef592296681b52fee1e6621d
kimdonghun <zhfptm12@naver.com>
2022-11-23 17:01:30 +0900

    Feat : add findAllByConditions sql Query

--- src/main/java/com/CnS/domain/course/repository/CourseRepository.java
+++ src/main/java/com/CnS/domain/course/repository/CourseRepository.java
@@ -1,6 +1,7 @@
 package com.CnS.domain.course.repository;
 
 import com.CnS.domain.course.entity.Course;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,5 +15,38 @@ public interface CourseRepository extends JpaRepository<Course, String> {
 
     @Query("select c from Course c where c.courseId in :coursesId")
     List<Course> findAllByCoursesId(@Param("coursesId") List<String> courses_Id);
+
+
+    // grade , courseNumber
+    @Query("select c from Course c where c.targetGrade= :grade and c.courseNumber = :courseNumber and c.name like :name and c.professor like :professor and c.major like :major")
+    List<Course> findAllByConditionsWithTwo(@Param("courseNumber") Integer courseNumber,
+                                     @Param("name") String name,
+                                     @Param("professor") String professor,
+                                     @Param("major") String major,
+                                     @Param("grade") Integer grade
+    );
+
+    // grade
+    @Query("select c from Course c where c.targetGrade = :grade and c.name like :name and c.professor like :professor and c.major like :major")
+    List<Course> findAllByConditionsWithGrade(
+            @Param("name") String name,
+            @Param("professor") String professor,
+            @Param("major") String major,
+            @Param("grade") Integer grade);
+
+    // courseNumber
+    @Query("select c from Course c where c.courseNumber = :courseNumber and c.name like :name and c.professor like :professor and c.major like :major")
+    List<Course> findAllByConditionsWithCourseNumber(@Param("courseNumber") Integer courseNumber,
+                                     @Param("name") String name,
+                                     @Param("professor") String professor,
+                                     @Param("major") String major
+                                     );
+
+    // x
+    @Query("select c from Course c where c.name like :name and c.professor like :professor and c.major like :major")
+    List<Course> findAllByConditionsWithZero(@Param("name") String name,
+                                     @Param("professor") String professor,
+                                     @Param("major") String major
+    );
 
 }
