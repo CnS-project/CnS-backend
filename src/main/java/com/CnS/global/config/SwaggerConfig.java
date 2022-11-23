1b24bcc6621e1ae8bb0f113fdafb7d866c0f5fd6
OhHongGi <5ghdrl@naver.com>
2022-11-23 20:05:33 +0900

    refactor: handle swagger resource

--- src/main/java/com/CnS/global/config/SwaggerConfig.java
+++ src/main/java/com/CnS/global/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.CnS.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,11 +14,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {  // Swagger
+public class SwaggerConfig implements WebMvcConfigurer {  // Swagger
 
     private static final String API_NAME = "CnS API";
     private static final String API_VERSION = "1";
     private static final String API_DESCRIPTION = "CnS API 명세서";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
     @Bean
     public Docket api() {
