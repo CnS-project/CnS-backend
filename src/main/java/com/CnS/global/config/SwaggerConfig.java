a67816ff429f15bcc7fd8044f83e3889cc5ef2d9
OhHongGi <5ghdrl@naver.com>
2022-11-23 20:13:44 +0900

    refactor: handle swagger resource

--- src/main/java/com/CnS/global/config/SwaggerConfig.java
+++ src/main/java/com/CnS/global/config/SwaggerConfig.java
@@ -22,7 +22,7 @@ public class SwaggerConfig implements WebMvcConfigurer {  // Swagger
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
+        registry.addResourceHandler("/swagger-ui.html")
             .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
             .addResourceLocations("classpath:/META-INF/resources/webjars/");
